5e59789b5c78a682e001632a2dd259954aca7fa7
Clewar <isaimonza99@gmail.com>
2020-06-15 08:46:14 -0500

    Added API usage

--- main.js
+++ main.js
@@ -76,37 +76,70 @@ function aceptar(){
             newDIV.innerHTML = txt;
             document.getElementById("mid-res").appendChild(newDIV)
         }
+        getWeather()
     }
     
 }
 
 function getWeather(){
-    /*switch(idioma) {
-        case "Inglés":
-          // code block
 
+    switch(selectedLangs[0]) {
+        case "Inglés":
+            //we(2643743)
+            async function weLondon(){
+                let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?id=2643743&appid=9bdedc0dd764e4bbf3e0e871ea6df339`)
+                let weather = await response.json()
+                return weather
+            }
+        
+            weLondon().then(weather => {
+                console.log(weather.main.temp)
+                document.getElementById("weather").innerHTML = `Weather at London: 
+                ${weather.weather[0].description} at ${parseInt(weather.main.temp-273.15,10)}C°`
+            })
           break;
         case "Francés":
-          // code block
-
+            //we(2968815)
+            async function weParis(){
+                let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?id=2968815&appid=9bdedc0dd764e4bbf3e0e871ea6df339`)
+                let weather = await response.json()
+                return weather
+            }
+        
+            weParis().then(weather => {
+                console.log(weather.main.temp)
+                document.getElementById("weather").innerHTML = `Weather at Paris: 
+                ${weather.weather[0].description} at ${parseInt(weather.main.temp-273.15,10)}C°`
+            })
           break;
         case "Español":
-            // code block
-
+            //we(3117735)
+            async function weMadrid(){
+                let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?id=3117735&appid=9bdedc0dd764e4bbf3e0e871ea6df339`)
+                let weather = await response.json()
+                return weather
+            }
+        
+            weMadrid().then(weather => {
+                console.log(weather.main.temp)
+                document.getElementById("weather").innerHTML = `Weather at Madrid: 
+                ${weather.weather[0].description} at ${parseInt(weather.main.temp-273.15,10)}C°`
+            })
             break;
         default:
-          // code block
+      }
 
-      }*/
-    async function we(){
-        let response = await fetch('https://api.openweathermap.org/data/2.5/weather?id=2643743&appid=9bdedc0dd764e4bbf3e0e871ea6df339')
+    /*async function we(id){
+        let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?id=${id}&appid=9bdedc0dd764e4bbf3e0e871ea6df339`)
         let weather = await response.json()
         return weather
     }
 
     we().then(weather => {
-        console.log(weather)
-        document.getElementById("weather").innerHTML = weather.weather[0].main
-    })    
+        console.log(weather.main.temp)
+        document.getElementById("weather").innerHTML = `Weather at London: 
+        ${weather.weather[0].description} at ${parseInt(weather.main.temp-273.15,10)}C°`
+    })*/    
   
 }
+
